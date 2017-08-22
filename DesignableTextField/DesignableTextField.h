f17d3743c096eba62fa40787da4be4c339dde0ae
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2017-08-22 14:36:44 +0200

    Initial Commit

---
+++ DesignableTextField/DesignableTextField.h
@@ -0,0 +1,19 @@
+//
+//  DesignableTextField.h
+//  DesignableTextField
+//
+//  Created by Oleksandr Pronin on 22.08.17.
+//  Copyright © 2017 Oleksandr Pronin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DesignableTextField.
+FOUNDATION_EXPORT double DesignableTextFieldVersionNumber;
+
+//! Project version string for DesignableTextField.
+FOUNDATION_EXPORT const unsigned char DesignableTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DesignableTextField/PublicHeader.h>
+
+
